2204f58f21acd887cd2a04f7ed169df6137355cd
IvanCastilloE <42181297+IvanCastilloE@users.noreply.github.com>
2020-11-03 15:33:23 -0700

    Push bueno

---
+++ practicaTableView/CeldaAlumnoController.swift
@@ -0,0 +1,16 @@
+//
+//  CeldaAlumnoController.swift
+//  practicaTableView
+//
+//  Created by Alumno on 11/3/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlumnoController: UITableViewCell{
+    @IBOutlet weak var lblNombre: UILabel!
+    @IBOutlet weak var lblMatricula: UILabel!
+    @IBOutlet weak var lblCarrera: UILabel!
+}
--- practicaTableView/ViewController.swift
+++ practicaTableView/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//Implementar protocolos de DataSource y Delegate
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var alumnos : [Alumno] = []
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 71
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "celdaAlumno") as! CeldaAlumnoController
+        celda.lblNombre.text = alumnos[indexPath.row].nombre
+        celda.lblMatricula.text = alumnos[indexPath.row].matricula
+        celda.lblCarrera.text = alumnos[indexPath.row].carrera
+        
+        return celda
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        alumnos.append(Alumno(nombre: "Jojose Gonzales", matricula: "9872", carrera: "Lic. Administracion"))
+        alumnos.append(Alumno(nombre: "Ana Grijalba", matricula: "9632", carrera: "Ing. Industrial"))
+        alumnos.append(Alumno(nombre: "Mario Mario", matricula: "6969", carrera: "Lic. Gastronomia"))
+        alumnos.append(Alumno(nombre: "Luigi Mario", matricula: "0420", carrera: "Ing. Gastronomia"))
     }
 
 
